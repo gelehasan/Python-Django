38d3bd63a9993ffe737ed110d6f6fc041b08c6c6
Nex Shadow <gellehussen@gmail.com>
2024-05-07 23:20:49 +0200

    added table for person

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class Hello(models.Model):
     text=models.CharField(max_length=200)
+
+
+class Person(models.model):
+    name=models.CharField(max_length=500, null=false, blank=False, db_index=True)
+    age=models.IntegerField(null=False, blank=True)
+
+    def __str__(self):
+        return self.name
+
