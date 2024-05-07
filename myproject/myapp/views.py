ee2b8fce4a42f6e3fbafff9dc36fed5d77fccc30
Nex Shadow <gellehussen@gmail.com>
2024-05-07 23:18:43 +0200

    added views for addresses

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,3 +5,10 @@ from .models import *
 def index(request):
     responseString= Hello.objects.all()[0]
     return render(request, 'helloworld/index.html', {'data':responseString})
+
+
+def simple_view(request):
+    addresses=Address.object.all()
+    first_address=addresses[0]
+    resident_name=str(first_address.resident)
+    return render(request, 'helloworld/simple.html', {"address":first_address, "name":resident_name})
