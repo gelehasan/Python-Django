6bf8640052c4ba771d71c1ef4cb34a4ab2fb545c
Gele hasan <gellehussen@gmail.com>
2024-05-23 17:32:51 +0200

    simplfying genes

--- secondProject/bioweb/bioapp/urls.py
+++ secondProject/bioweb/bioapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns= [
     path('', views.index, name='index'),
     path('gene/<int:pk>', views.gene, name='gene'),
     path('delete/<int:pk>', views.delete,name='delete'),
-    path('create_ec/', views.create_ec, name='create_ec')
+    path('create_ec/', views.create_ec, name='create_ec'),
+    path('create_gene/', views.create_gene, name='create_gene'),
+    path('update/<int:pk>', views.GeneUpdate.as_view(), name='update')
 ]
--- secondProject/bioweb/bioapp/views.py
+++ secondProject/bioweb/bioapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import *
 from .forms import *  
+from django.views.generic import UpdateView
 # Create your views here.
 
 def index(request):
@@ -17,6 +18,17 @@ def gene(request,pk):
     gene.save()
     return render(request,'bioapp/gene.html', {'gene':gene})
 
+class GeneUpdate(UpdateView):
+    model=Gene
+    fields=['gene_id', 'entity', 'start', 'stop', 'sense', 'start_codon', 'sequencing', 'ec']
+    template_name_suffix='_update_form'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+
+    
 
 def delete (request, pk):
     GeneAttributeLink.objects.filter(gene_id=pk).delete()
@@ -24,6 +36,7 @@ def delete (request, pk):
     return HttpResponseRedirect("/")
 
 def create_ec(request):
+    master_genes=Gene.objects.all()
     if request.method == 'POST':
         form = ECForm(request.POST)
         if form.is_valid():
@@ -34,4 +47,20 @@ def create_ec(request):
     else:
         ecs = Ec.objects.all()
         form = ECForm()
-    return render(request, 'bioapp/ec.html', {'form': form, 'ecs': ecs})
+    return render(request, 'bioapp/ec.html', {'form': form, 'ecs': ecs, 'master_genes':  master_genes})
+
+
+def create_gene(request):
+    if request.method == 'POST':
+        form = GeneForm(request.POST)
+        if form.is_valid():
+            gene = form.save()
+            return HttpResponseRedirect('/create_gene/')
+        else:
+            return render(request, 'genedata/create_gene.html', {'error': 'failed','master_genes': master_genes,'form': form})
+    else:
+        master_genes = Gene.objects.all()
+        form = GeneForm()
+
+    
+    return render(request, 'bioapp/create_gene.html', {'form': form, 'master_genes': master_genes})
