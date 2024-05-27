96b935332d6f514a1ffd97c0e63391e0148af7fe
Gele hasan <gellehussen@gmail.com>
2024-05-27 22:55:34 +0200

    finilzed crud operation

--- secondProject/bioweb/bioapp/api.py
+++ secondProject/bioweb/bioapp/api.py
@@ -24,6 +24,18 @@ def gen_detail(request, pk=None):
     if request.method == 'GET':
         serializer = GeneSerializer(gene)
         return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = GeneSerializer(gene, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        gene.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
 
 @api_view(['GET'])
 def gene_list(request):
