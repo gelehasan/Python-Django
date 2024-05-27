1a7805157146d760ce5840b2b8557f2914c56032
Gele hasan <gellehussen@gmail.com>
2024-05-27 22:52:45 +0200

    applied post

--- secondProject/bioweb/bioapp/api.py
+++ secondProject/bioweb/bioapp/api.py
@@ -8,16 +8,22 @@ from .models import *
 from .serializers import *
 
 @api_view(['GET', 'POST'])
-def gen_detail(request, pk):
+def gen_detail(request, pk=None):
+    if request.method == 'POST':
+        serializer = GeneSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
     try:
         gene = Gene.objects.get(pk=pk)
     except Gene.DoesNotExist:
         return HttpResponse(status=404)
     
-    if request.method=='GET':
-        serializer =GeneSerializer(gene)
+    if request.method == 'GET':
+        serializer = GeneSerializer(gene)
         return Response(serializer.data)
-
 
 @api_view(['GET'])
 def gene_list(request):
--- secondProject/bioweb/bioapp/serializers.py
+++ secondProject/bioweb/bioapp/serializers.py
@@ -1,9 +1,46 @@
 from rest_framework import serializers
 from .models import *
 
+class ECSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ec
+        fields = ['id', 'ec_name']
+
+class SequencingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sequencing
+        fields = ['id', 'sequencing_factory', 'sequencing_location']
+
+ 
 
 class GeneSerializer(serializers.ModelSerializer):
+    ec = ECSerializer()
+    sequencing = SequencingSerializer()
+    
     class Meta:
-        model= Gene
-        fields=['gene_id', 'entity', 'start', 'stop', 'sense', 'start_codon', 'sequencing', 'ec']
+        model = Gene
+        fields = ['gene_id', 'entity', 'start', 'stop', 'sense', 'start_codon', 'ec', 'sequencing']
 
+    def create(self, validated_data):
+        ec_data = self.initial_data.get('ec')
+        seq_data = self.initial_data.get('sequencing')
+        
+        ec = Ec.objects.get(pk=ec_data['id'])
+        sequencing = Sequencing.objects.get(pk=seq_data['id'])
+        
+        validated_data.pop('ec')
+        validated_data.pop('sequencing')
+
+        gene = Gene.objects.create(
+            **validated_data,
+            ec=ec,
+            sequencing=sequencing
+        )
+        gene.save()
+        return gene
+
+
+class GeneListSerializer(serializers.ModelSerializer):
+     class Meta:
+        model= Gene
+        fields=['id', 'gene_id']
