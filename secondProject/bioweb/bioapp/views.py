01b1c3526773e636f52cce871d00bc8024f898b0
Gele hasan <gellehussen@gmail.com>
2024-06-29 23:56:38 +0200

    removed comment lines of codes in view

--- secondProject/bioweb/bioapp/views.py
+++ secondProject/bioweb/bioapp/views.py
@@ -7,11 +7,6 @@ from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
-# Create your views here.
-
-#def index(request):
-  #  genes=Gene.objects.all()
-   # return render(request, 'bioapp/index.html', {'genes':genes})
 
 class GeneList(ListView):
     model= Gene
@@ -19,14 +14,7 @@ class GeneList(ListView):
     template_name='bioapp/index.html'
 
 
-"""
-def gene(request,pk):
-    gene=Gene.objects.get(pk=pk)
-    gene.access +=1
-    print("Gene Record: " , pk, " Access count : ", str(gene.access))
-    gene.save()
-    return render(request,'bioapp/gene.html', {'gene':gene})
-"""
+
 
 class GeneDetail(DetailView):
     model=Gene
@@ -45,11 +33,6 @@ class GeneUpdate(UpdateView):
 
     
 
-"""def delete (request, pk):
-    GeneAttributeLink.objects.filter(gene_id=pk).delete()
-    Gene.objects.filter(pk=pk).delete()
-    return HttpResponseRedirect("/")
-"""
 
 class GeneDelete(DeleteView):
     model=Gene
@@ -70,22 +53,6 @@ def create_ec(request):
         form = ECForm()
     return render(request, 'bioapp/ec.html', {'form': form, 'ecs': ecs, 'master_genes':  master_genes})
 
-"""
-def create_gene(request):
-    if request.method == 'POST':
-        form = GeneForm(request.POST)
-        if form.is_valid():
-            gene = form.save()
-            return HttpResponseRedirect('/create_gene/')
-        else:
-            return render(request, 'bioapp/create_gene.html', {'error': 'failed','master_genes': master_genes,'form': form})
-    else:
-        master_genes = Gene.objects.all()
-        form = GeneForm()
-
-    
-    return render(request, 'bioapp/create_gene.html', {'form': form, 'master_genes': master_genes})
-"""
 
 class GeneCreate(CreateView):
     model=Gene
