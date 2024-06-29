3cd355756ccb1b1c5bc02309b0bf2f0dfa30af8c
Gele hasan <gellehussen@gmail.com>
2024-06-29 23:56:23 +0200

    cleaned api view

--- secondProject/bioweb/bioapp/api.py
+++ secondProject/bioweb/bioapp/api.py
@@ -9,48 +9,6 @@ from .serializers import *
 from rest_framework import generics
 from rest_framework import mixins
 
-"""
-@api_view(['GET', 'POST'])
-def gen_detail(request, pk=None):
-    if request.method == 'POST':
-        serializer = GeneSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-    try:
-        gene = Gene.objects.get(pk=pk)
-    except Gene.DoesNotExist:
-        return HttpResponse(status=404)
-    
-    if request.method == 'GET':
-        serializer = GeneSerializer(gene)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = GeneSerializer(gene, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        gene.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-"""
-
-"""
-@api_view(['GET'])
-def gene_list(request):
-    try:
-        gene = Gene.objects.all()
-    except Gene.DoesNotExist:
-        return HttpResponse(status=404)
-    
-    if request.method=='GET':
-        serializer =GeneSerializer(gene, many=True)
-        return Response(serializer.data)
-"""
 class GeneDetail(mixins.CreateModelMixin,
                  mixins.RetrieveModelMixin,
                  mixins.UpdateModelMixin,
